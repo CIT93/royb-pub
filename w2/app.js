188b2a3e185fc539c30067306f1ed0624b42b293
roybean1988 <bopity123@gmail.com>
2022-08-18 23:34:10 +0000

    index.html

---
+++ w2/app.js
@@ -0,0 +1,29 @@
+// <b> bold writing
+const displayOnPage = function(text) {
+    let newParagraph = document.createElement('p')
+    newParagraph.innerHTML = text
+    let outputDiv = docuement.getElementById('output')
+    outputDiv.append(newParagraph)
+}
+
+// Daily morning planning + Decisions
+// Determining if I have time to prepare lunch or grab something on the way to work
+
+//Global Variables
+let isHungry = true
+let isTired = false
+let isMonday = true
+let isUpEarly = true
+let isTime = 4
+let isLateforwork = isTime <= 5
+
+showOnPage('<b>Global Variables</b>')
+showOnPage('Am I late for work' + isLateforwork)
+
+// if and if else statements
+// determing if I have homework and if im in the mood for it or to tired today.
+if (isTired) {
+    c
+}
+
+
