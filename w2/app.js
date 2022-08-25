5e7cdfe70a4f2de22d48c4c5ee0399eee40d53a0
roybean1988 <bopity123@gmail.com>
2022-08-25 22:18:37 +0000

    is homework due soon

--- w2/app.js
+++ w2/app.js
@@ -1,50 +1,46 @@
 // <b> bold writing
 const showOnPage = function (text) {
-    let newParagraph = document.createElement('p')
-    newParagraph.innerHTML = text;
-    let outputDiv = document.getElementById('output')
-    outputDiv.append(newParagraph)
-  }
+  let newParagraph = document.createElement("p");
+  newParagraph.innerHTML = text;
+  let outputDiv = document.getElementById("output");
+  outputDiv.append(newParagraph);
+};
 
 // Daily morning planning + Decisions
 // Determining if I have time to prepare breakfast or grab something on the way to work
 
 //Global Variables
 
-let isTired = true
-let isUpEarly = true
+let isTired = true;
+let isUpEarly = true;
 let isHaveMilk = true;
 let isHungry = true;
 
-showOnPage('<b>Global Variables</b>')
+showOnPage("<b>Global Variables</b>");
 
-showOnPage('Am I tired,')
-if (isTired = true) { 
-    showOnPage('yes, but am I up early')
-
-} else { 
-    showOnPage('no, I should eat')
+showOnPage("Am I tired,");
+if ((isTired = true)) {
+  showOnPage("yes, but am I up early");
+} else {
+  showOnPage("no, I should eat");
 }
 
 // if and if else statements
 // determing if I have time to eat or not before work
-if (isUpEarly = true) {
-    showOnPage('Yes....')
+if ((isUpEarly = true)) {
+  showOnPage("Yes....");
 } else {
-    showOnPage('No better just find food quick and head to work...')
+  showOnPage("No better just find food quick and head to work...");
 }
 // if im up early then I have time to make food or else i can grab something on the way to work
-if (isHungry = true) {
-
-    showOnPage(' Do I have milk?')
- } else {
-        showOnPage(' Grab a snack for later for work')
-    
+if ((isHungry = true)) {
+  showOnPage(" Do I have milk?");
+} else {
+  showOnPage(" Grab a snack for later for work");
 }
 // if i have milk i can just do cereal else i can just grab a snack
-if (isHaveMilk = true) {
-    showOnPage('Yes....Grab some ceral and eat before work!')
+if ((isHaveMilk = true)) {
+  showOnPage("Yes....Grab some ceral and eat before work!");
 } else {
-    showOnPage('No...just head to work and grab a snack')
+  showOnPage("No...just head to work and grab a snack");
 }
-
---
+++ w3/app2.js
@@ -0,0 +1,39 @@
+// Displaying on page functions
+const showOnPage = function (text) {
+    let newParagraph = document.createElement("p")
+    newParagraph.innerHTML = text
+    let outputDiv = document.getElementById("output")
+    outputDiv.append(newParagraph)
+}
+// This is a constant battle for me, I check calendar everyday and try to do all my homework but sometimes my mind just turns off unless its due right now.
+let doHomework = function (Due = 1) {
+    if (Due >= 2 && Due <= 99) {
+     return showOnPage(isDuein (3));
+    } else {
+      return showOnPage('Relax and work on it when you can.');
+    }
+  }
+  
+  
+  
+  // With my ADHD I can't focus long periods on homework unless its immediatly due, so I tend to push things off until the end or rush and finish all of it ahead of time.
+  // Made something easy similar to our calendars just to show if I have homework due.
+  let isDuein = function (_Days) {
+    // if else statement determining if I have to rush to do homework or can take my time and spread it out.
+    if (isDuein >= 1 && isDuein <= 3) {
+      let isHomework = 'You need to do homework now.';
+      let isDays = 'homework due date is coming up!';
+      return `you have homework due, ${isHomework}, because the ${isDays}.`;
+    } else if (isDuein >= 1 && isDuein <= 2) {
+      let isNow = ' it is due soon, hurry and do the homework!';
+      return `Looks like you have homework, ${isNow}.`;
+    } else if (isDuein >= 4 && temp <= 999) {
+      let isOk = 'you have a few more days';
+      let doRelax = 'just relax';
+      let isAlright = `do homework, you can but it will be alright...but don't rush it.`;
+      return `Nothing is due right now, ${isOk}, to ${doRelax} and if you want to ${isAlright}.`;
+    } else {
+      return 'Time to panic, what did you do with your time?!';
+    }
+  };
+  doHomework();
