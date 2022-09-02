1e4a99f863746ea1c50f41d47c42f9c52f24ba43
roybean1988 <bopity123@gmail.com>
2022-09-02 02:29:56 +0000

    w4

--- w4/apps.js
+++ w4/apps.js
@@ -47,17 +47,3 @@ isDaysUntilDue.determineDay(4);
 showOnPage(`Things to do today.  ${isDaysUntilDue.whatToDo} `);
 isDaysUntilDue.determineDay(5);
 showOnPage(`Things to do today.  ${isDaysUntilDue.whatToDo} `);
-
-// cant figure out why it only displays one: not the rest... ive changed everything I can think of...
-// need to figure out how to use this instead of days so i can actually use todays date
-/* var tomorrow = new Date();
-tomorrow.setDate(tomorrow.getDate() + 1); 
-today=new Date();
-let due = new Date(today.getFullYear(), 3, 4);
-if (today.getMonth()==3 && today.getDate()>25) 
-{
-due.setFullYear(due.getFullYear()+1); 
-}  
-let one_day=1000*60*60*24;
-console.log(Math.ceil((due.getTime()-today.getTime())/(one_day))+
-" days left until Christmas!");
